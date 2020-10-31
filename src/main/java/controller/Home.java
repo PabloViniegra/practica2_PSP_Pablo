ad1d51427e355f8c5a4f7125f9d070e40cd69c7a
Pablo Viniegra <pablovpmadrid@gmail.com>
2020-10-31 16:30:13 +0100

    La aplicación empieza a hacer su tarea. Se ha refinado los bucles de los Threads. Ahora se nota la diferencia de ejecución de tiempo de la tarea entre su ejecución concurrente y su ejecución paralela. Sin embargo, se sigue sin temporizar la suma de los sueldos.

--- src/main/java/controller/Home.java
+++ src/main/java/controller/Home.java
@@ -1,44 +1,62 @@
 package controller;
 
+import views.View;
+
 import java.util.ArrayList;
 
 public class Home {
     static final int THREADS = 5;
+
     public static void main(String[] args) {
         DatabaseManagement db = new DatabaseManagement();
+        View.menu();
         db.readFromDatabaseInMainThread();
-        ArrayList <DatabaseManagement> mydbs = new ArrayList<>();
+        ArrayList<DatabaseManagement> mydbs = new ArrayList<>();
         System.out.println("Ahora 5 con hilos");
         DatabaseManagement dbThreads;
         int numberOfRegisters = db.requestNumberOfRegisters();
-        int initial, last, total = 0;
+        int initial, last;
         long timeInit = System.currentTimeMillis();
-        for (int i = 0; i < THREADS; i++) {
-            initial = (numberOfRegisters/THREADS) * (i-1);
+        for (int i = 0; i <= THREADS; i++) {
+            initial = (numberOfRegisters / THREADS) * (i - 1);
             if (i == THREADS) {
                 last = numberOfRegisters;
-            }
-            else {
-                last = (numberOfRegisters/THREADS) * i;
+            } else {
+                last = (numberOfRegisters / THREADS) * i;
             }
 
             if (i == 1) {
                 initial = 1;
-            }
-            else {
-                last = (numberOfRegisters/THREADS) * i;
+            } else {
+                last = (numberOfRegisters / THREADS) * i;
             }
 
-            dbThreads = new DatabaseManagement(initial,last);
+            dbThreads = new DatabaseManagement(initial, last);
             dbThreads.start();
             mydbs.add(dbThreads);
+            try {
+                dbThreads.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
-        System.out.println("Duración: " + (timeInit - System.currentTimeMillis()) + " milisegundos");
-       long timeInitSume = System.currentTimeMillis();
-        for (DatabaseManagement mydb : mydbs) {
-            total+=mydb.getTotal();
+        long timeFinal = System.currentTimeMillis();
+        long time = timeFinal - timeInit;
+        System.out.println("Duración: " + time + " milisegundos");
+        getDurationSume(mydbs);
+
+    }
+
+    public static void getDurationSume(ArrayList<DatabaseManagement> myArray) {
+        int total = 0;
+        long timeInitSume = System.currentTimeMillis();
+        for (DatabaseManagement mydb : myArray) {
+            total += mydb.getTotal();
         }
-        System.out.println("Duración de la suma: " + (timeInitSume - System.currentTimeMillis()) + " milisegundos");
+        System.out.println("Suma de sueldos: " + total);
+        long timeFinalSume = System.currentTimeMillis();
+        long timeSume = timeInitSume - timeFinalSume;
+        System.out.println("Duración de la suma: " + timeSume + " milisegundos");
     }
 }
